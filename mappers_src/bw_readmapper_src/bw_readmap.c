a0902c9b1fae74490aa194788bd4e685ded093e9
Thomas Mailund <mailund@birc.au.dk>
2018-03-12 20:33:48 +0100

    Fixed re-allocation bug.
    Resolves #2.

--- mappers_src/bw_readmapper_src/bw_readmap.c
+++ mappers_src/bw_readmapper_src/bw_readmap.c
@@ -64,23 +64,29 @@ int main(int argc, char * argv[])
 {
     const char *prog_name = argv[0];
 
-    int opt; bool preprocess = false; int edit_dist = 0;
+    int opt;
+    bool preprocess = false;
+    bool validate = false;
+    int edit_dist = 0;
     static struct option longopts[] = {
         { "help",        no_argument,            NULL,           'h' },
-        { "preprocess",  required_argument,      NULL,           'p' },
+        { "preprocess",  no_argument,            NULL,           'p' },
+        { "validate",    no_argument,            NULL,           'v' },
         { "distance",    required_argument,      NULL,           'd' },
-        { NULL,         0,                      NULL,            0  }
+        { NULL,         0,                       NULL,            0  }
     };
-    while ((opt = getopt_long(argc, argv, "hpd:", longopts, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "hpvd:", longopts, NULL)) != -1) {
         switch (opt) {
             case 'h':
                 printf("Usage: %s -p | --preprocess ref.fa\n"
+                       "       %s -v | --validate ref.fa\n"
                        "       %s [-d distance] ref.fa reads.fq\n",
-                       prog_name, prog_name);
+                       prog_name, prog_name, prog_name);
                 printf("Options:\n");
                 printf("\t-h | --help:\t\t Show this message.\n");
                 printf("\t-d | --distance:\t Maximum edit distance for the search.\n");
                 printf("\t-p | --preprocess:\t Preprocess a reference genome.\n");
+                printf("\t-v | --validate:\t Checks that preprocessed data matches the computed tables reference genome.\n");
                 printf("\n\n");
                 return EXIT_SUCCESS;
                 
@@ -88,6 +94,10 @@ int main(int argc, char * argv[])
                 preprocess = true;
                 break;
 
+            case 'v':
+                validate = true;
+                break;
+                
             case 'd':
                 edit_dist = atoi(optarg);
                 break;
@@ -95,8 +105,9 @@ int main(int argc, char * argv[])
             default:
                 fprintf(stderr,
                         "Usage: %s -p | --preprocess ref.fa\n"
+                        "       %s -v | --validate ref.fa\n"
                         "       %s [-d distance] ref.fa reads.fq\n",
-                        prog_name, prog_name);
+                        prog_name, prog_name, prog_name);
                 return EXIT_FAILURE;
         }
     }
@@ -107,8 +118,9 @@ int main(int argc, char * argv[])
         if (argc != 1) {
             fprintf(stderr,
                     "Usage: %s -p | --preprocess ref.fa\n"
+                    "       %s -v | --validate ref.fa\n"
                     "       %s [-d distance] ref.fa reads.fq\n",
-                    prog_name, prog_name);
+                    prog_name, prog_name, prog_name);
             return EXIT_FAILURE;
         }
         
@@ -128,13 +140,103 @@ int main(int argc, char * argv[])
         struct suffix_array_records *sa_records =
             build_suffix_array_records(records);
         
+#if 0
+        fprintf(stderr, "suffix array structures after built.\n");
+        for (int i = 0; i < records->names->used; i++) {
+            struct suffix_array *sa = sa_records->suffix_arrays[i];
+            const char *string = records->sequences->strings[i];
+          
+            fprintf(stderr, "suffix array:\n");
+            for (int i = 0; i < sa->length; i++) {
+                fprintf(stderr, "sa[%d] == %4lu %s\n",
+                        i, sa->array[i], string + sa->array[i]);
+            }
+
+            for (size_t i = 0; i < sa->c_table_no_symbols; i++) {
+                char symbol = sa->c_table_symbols[i];
+                printf("O(%c,) =", (symbol == 0) ? '$' : symbol);
+                for (size_t j = 0; j < sa->length; ++j) {
+                    size_t idx = o_table_index(sa, symbol, j);
+                    printf(" %lu", sa->o_table[idx]);
+                }
+                printf("\n");
+            }
+
+        }
+#endif
+        
         write_suffix_array_records(sa_records, records, argv[0]);
         
         fprintf(stderr, "Clean up suffic array records.\n");
         delete_suffix_array_records(sa_records);
-        fprintf(stderr, "Celan up FASTA records.\n");
+        fprintf(stderr, "Clean up FASTA records.\n");
         delete_fasta_records(records);
         fprintf(stderr, "All done.\n");
+       
+    } else if (validate) {
+        
+        if (argc != 1) {
+            fprintf(stderr,
+                    "Usage: %s -p | --preprocess ref.fa\n"
+                    "       %s -v | --validate ref.fa\n"
+                    "       %s [-d distance] ref.fa reads.fq\n",
+                    prog_name, prog_name, prog_name);
+            return EXIT_FAILURE;
+        }
+        
+        FILE *fasta_file = fopen(argv[0], "r");
+        if (!fasta_file) {
+            fprintf(stderr, "Could not open %s.\n", argv[0]);
+            return EXIT_FAILURE;
+        }
+        
+        struct fasta_records *fasta_records = empty_fasta_records();
+        if (0 != read_fasta_records(fasta_records, fasta_file)) {
+            fprintf(stderr, "Could not read FASTA file.\n");
+            return EXIT_FAILURE;
+        }
+        fclose(fasta_file);
+        
+        struct suffix_array_records *built_sa_records =
+            build_suffix_array_records(fasta_records);
+        
+        struct suffix_array_records *loaded_sa_records = empty_suffix_array_records();
+        
+        if (0 != read_suffix_array_records(loaded_sa_records,
+                                           fasta_records,
+                                           argv[0])) {
+            fprintf(stderr, "Could not read suffix arrays.\n");
+            delete_fasta_records(fasta_records);
+            delete_suffix_array_records(built_sa_records);
+            delete_suffix_array_records(loaded_sa_records);
+            return EXIT_FAILURE;
+        }
+        
+        fprintf(stdout, "Checking that suffix arrays are the same.\n");
+        size_t no_records = fasta_records->names->used;
+        for (size_t i = 0; i < no_records; ++i) {
+            const char *seq_name = fasta_records->names->strings[i];
+            fprintf(stdout, "...comparing for %s.\n", seq_name);
+            struct suffix_array *built_sa = built_sa_records->suffix_arrays[i];
+            struct suffix_array *loaded_sa = loaded_sa_records->suffix_arrays[i];
+            if (built_sa->length != loaded_sa->length) {
+                fprintf(stderr, "The suffix arrays disagree on length. %lu != %lu\n",
+                        built_sa->length, loaded_sa->length);
+                return EXIT_FAILURE;
+            }
+            for (size_t j = 0; j < built_sa->length; j++) {
+                if (built_sa->array[j] != loaded_sa->array[j]) {
+                    fprintf(stderr, "The suffix arrays disagree on index %lu : %lu != %lu\n",
+                            j, built_sa->array[j], loaded_sa->array[j]);
+                    return EXIT_FAILURE;
+                }
+            }
+        }
+        
+        delete_fasta_records(fasta_records);
+        delete_suffix_array_records(built_sa_records);
+        delete_suffix_array_records(loaded_sa_records);
+        return EXIT_SUCCESS;
         
     } else {
         if (argc != 2) {
--- mappers_src/bw_readmapper_src/suffix_array_records.c
+++ mappers_src/bw_readmapper_src/suffix_array_records.c
@@ -53,10 +53,18 @@ void delete_suffix_array_records(struct suffix_array_records *records)
     free(records);
 }
 
-static char *make_file_name(const char *prefix, const char *suffix) {
+static char *make_file_name(const char *prefix,
+                            const char *suffix,
+                            const char *seq_suffix) {
     size_t prefix_length = strlen(prefix);
     size_t suffix_length = strlen(suffix);
     size_t string_length = prefix_length + 1 + suffix_length + 1;
+
+    size_t seq_suffix_length = 0;
+    if (seq_suffix) {
+        seq_suffix_length = strlen(seq_suffix);
+        string_length += seq_suffix_length + 1;
+    }
     
     char *buffer = (char*)malloc(string_length);
     char *c = buffer;
@@ -67,7 +75,15 @@ static char *make_file_name(const char *prefix, const char *suffix) {
     for (int i = 0; i < suffix_length; i++, c++) {
         *c = suffix[i];
     }
+    if (seq_suffix) {
+        *c = '.'; c++;
+        for (int i = 0; i < seq_suffix_length; i++, c++) {
+            *c = seq_suffix[i];
+        }
+    }
     *c = 0;
+    
+    assert(strlen(buffer) + 1 == string_length);
     
     return buffer;
 }
@@ -76,25 +92,25 @@ int write_suffix_array_records(struct suffix_array_records *records,
                                struct fasta_records *fasta_records,
                                const char *filename_prefix)
 {
-    fprintf(stderr, "Writig preprocessed data to files.\n");
-    char *filename = make_file_name(filename_prefix, "suffix_arrays");
-    fprintf(stderr, "writing suffix array to %s.\n", filename);
-    FILE *sa_file = fopen(filename, "w");
-    for (int i = 0; i < records->names->used; i++) {
-        struct suffix_array *sa = records->suffix_arrays[i];
-        fprintf(sa_file, "%s", records->names->strings[i]);
-        fprintf(sa_file, " %lu", sa->length);
-        for (int j = 0; j < sa->length; j++) {
-            fprintf(sa_file, " %lu", sa->array[j]);
-        }
-        fprintf(sa_file, "\n");
-    }
-    fclose(sa_file);
-    free(filename);
+    fprintf(stderr, "Writing preprocessed data to files.\n");
     
-    filename = make_file_name(filename_prefix, "c_tables");
+    for (int i = 0; i < records->names->used; i++) {
+        const char *seq_name = records->names->strings[i];
+        struct suffix_array *sa = records->suffix_arrays[i];
+        
+        char *filename = make_file_name(filename_prefix, "suffix_arrays", seq_name);
+        
+        fprintf(stderr, "writing suffix array to %s.\n", filename);
+        FILE *file = fopen(filename, "wb");
+        free(filename);
+        
+        fwrite(sa->array, sizeof(size_t), sa->length, file);
+        fclose(file);
+    }
+    
+    char *filename = make_file_name(filename_prefix, "c_tables", 0);
     fprintf(stderr, "writing c-table to %s.\n", filename);
-    sa_file = fopen(filename, "w");
+    FILE *sa_file = fopen(filename, "w");
     for (int i = 0; i < records->names->used; i++) {
         size_t *c_table = records->suffix_arrays[i]->c_table;
         size_t  c_table_no_symbols = records->suffix_arrays[i]->c_table_no_symbols;
@@ -114,80 +130,62 @@ int write_suffix_array_records(struct suffix_array_records *records,
     }
     fclose(sa_file);
     free(filename);
-    
-    filename = make_file_name(filename_prefix, "o_tables");
-    fprintf(stderr, "writing o-table to %s.\n", filename);
-    sa_file = fopen(filename, "w");
+
     for (int i = 0; i < records->names->used; i++) {
+        const char *seq_name = records->names->strings[i];
+        
         size_t *o_table = records->suffix_arrays[i]->o_table;
         size_t  o_table_size = records->suffix_arrays[i]->c_table_no_symbols * records->suffix_arrays[i]->length;
         
-        assert(o_table);
-        assert(o_table_size);
+        char *filename = make_file_name(filename_prefix, "o_tables", seq_name);
+        fprintf(stderr, "writing o-table to %s.\n", filename);
         
-        fprintf(sa_file, "%s", records->names->strings[i]);
-        fprintf(sa_file, " %lu", o_table_size);
-        for (size_t i = 0; i < o_table_size; i++) {
-            fprintf(sa_file, " %lu", o_table[i]);
-        }
-        fprintf(sa_file, "\n");
+        FILE *file = fopen(filename, "wb");
+        free(filename);
+        fwrite(o_table, sizeof(size_t), o_table_size, file);
+        fclose(file);
+        
     }
-    fclose(sa_file);
-    free(filename);
     
     fprintf(stderr, "Done.\n");
     
     return 0;
 }
 
-#define NAME_BUFFER_SIZE 1024
 static int read_o_table_records(struct suffix_array_records *records,
                                 struct fasta_records *fasta_records,
                                 const char *filename_prefix)
 {
-    char *filename = make_file_name(filename_prefix, "o_tables");
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        fprintf(stderr, "Could not open file %s.\n", filename);
-        exit(1);
-    }
-    fprintf(stderr, "Reading o-table from %s.\n", filename);
+    assert(records->suffix_arrays != 0);
     
-    int no_records = fasta_records->names->used;
-    for (int i = 0; i < no_records; i++) {
-        char seq_name[NAME_BUFFER_SIZE];
-        fscanf(file, "%1024s", (char*)&seq_name);
-        if (strcmp(seq_name, fasta_records->names->strings[i]) != 0) {
-            fprintf(stderr, "The preprocessed o-table sequence read is %s while the FASTA record is %s. This is an error!\n",
-                    seq_name, fasta_records->names->strings[i]);
-            return 1;
-        }
-        fprintf(stderr, "reading o-table for sequence %s.\n", seq_name);
-
-        size_t no_symbols = records->suffix_arrays[i]->c_table_no_symbols;
-        size_t string_length = records->suffix_arrays[i]->length;
-        size_t o_table_size;
-        fscanf(file, "%lu", &o_table_size);
-        fprintf(stderr, "... size %lu [%lu x %lu].\n",
-                o_table_size, no_symbols, string_length);
-        if (o_table_size != no_symbols * string_length) {
-            fprintf(stderr, "Unexpected size of o-table!\n");
+    for (int i = 0; i < fasta_records->names->used; i++) {
+        const char *seq_name = fasta_records->names->strings[i];
+        struct suffix_array *sa = records->suffix_arrays[i];
+        char *filename = make_file_name(filename_prefix, "o_tables", seq_name);
+        
+        FILE *file = fopen(filename, "rb");
+        if (!file) {
+            fprintf(stderr, "Could not open file %s.\n", filename);
             exit(1);
         }
+        fprintf(stderr, "reading o-table from %s.\n", filename);
         
-        size_t *o_table = malloc(sizeof(size_t)*o_table_size);
-        for (size_t i = 0; i < o_table_size; i++) {
-            size_t val;
-            fscanf(file, "%lu", &val);
-            o_table[i] = val;
+        size_t  o_table_size = records->suffix_arrays[i]->c_table_no_symbols * records->suffix_arrays[i]->length;
+        fprintf(stderr, "...allocating o-table size: [%lu x %lu] (%lu)\n",
+                sa->c_table_no_symbols, sa->length,
+                o_table_size);
+        assert(o_table_size > 0);
+        sa->o_table = malloc(o_table_size * sizeof(size_t));
+        if (!sa->o_table) {
+            fprintf(stderr, "...could not allocate memory for o-table.\n");
+            exit(1);
         }
+        fread(sa->o_table, sizeof(size_t), o_table_size, file);
         
-        if (records->suffix_arrays[i]->o_table)
-            free(records->suffix_arrays[i]->o_table);
-        records->suffix_arrays[i]->o_table = o_table;
-        
+        fclose(file);
+        free(filename);
+
 #if 0
-        struct suffix_array *sa = records->suffix_arrays[i];
         for (size_t i = 0; i < sa->c_table_no_symbols; i++) {
             char symbol = sa->c_table_symbols[i];
             printf("O(%c,) =", (symbol == 0) ? '$' : symbol);
@@ -208,11 +206,12 @@ static int read_o_table_records(struct suffix_array_records *records,
     return 0;
 }
 
+#define NAME_BUFFER_SIZE 1024
 static int read_c_table_records(struct suffix_array_records *records,
                                 struct fasta_records *fasta_records,
                                 const char *filename_prefix)
 {
-    char *filename = make_file_name(filename_prefix, "c_tables");
+    char *filename = make_file_name(filename_prefix, "c_tables", 0);
     FILE *file = fopen(filename, "r");
     if (!file) {
         fprintf(stderr, "Could not open file %s.\n", filename);
@@ -282,76 +281,48 @@ static int read_c_table_records(struct suffix_array_records *records,
     return 0;
 }
 
-
-
 int read_suffix_array_records(struct suffix_array_records *records,
                               struct fasta_records *fasta_records,
                               const char *filename_prefix)
 {
-    char *filename = make_file_name(filename_prefix, "suffix_arrays");
-    FILE *file = fopen(filename, "r");
-    
-    if (!file) {
-        fprintf(stderr, "Could not open file %s.\n", filename);
-        exit(1);
-    }
-    fprintf(stderr, "Reading suffix arrays from %s.\n", filename);
-    
     int no_records = fasta_records->names->used;
     assert(records->suffix_arrays == 0);
     
     records->suffix_arrays =
         (struct suffix_array**)malloc(sizeof(struct suffix_array*) * no_records);
     
-    char seq_name[NAME_BUFFER_SIZE];
     for (int i = 0; i < fasta_records->names->used; i++) {
-        fscanf(file, "%1024s", (char*)&seq_name);
-        if (strcmp(seq_name, fasta_records->names->strings[i]) != 0) {
-            fprintf(stderr, "The preprocessed sequence read is %s while the FASTA record is %s. This is an error!\n",
-                    seq_name, fasta_records->names->strings[i]);
-            return 1;
-        }
-        fprintf(stderr, "reading suffix array for sequence %s.\n",
-                seq_name);
-        add_string_copy(records->names, seq_name);
-        struct suffix_array *sa = empty_suffix_array();
-        size_t length = fasta_records->seq_sizes->sizes[i] + 1;
-        size_t file_length;
-        fscanf(file, "%lu", &file_length);
-        if (length != file_length) {
-            fprintf(stderr, "We expected to see a suffix array of length %lu but find one of length %lu.\n", length, file_length);
+        const char *seq_name = fasta_records->names->strings[i];
+        size_t seq_length = fasta_records->seq_sizes->sizes[i];
+        struct suffix_array *sa =
+            records->suffix_arrays[i] =
+            empty_suffix_array();
+        
+        char *filename = make_file_name(filename_prefix, "suffix_arrays", seq_name);
+        FILE *file = fopen(filename, "rb");
+        if (!file) {
+            fprintf(stderr, "Could not open file %s.\n", filename);
             exit(1);
         }
-        sa->length = length;
-        sa->array = malloc(sizeof(size_t) * length);
-        for (int j = 0; j < length; j++) {
-            size_t index;
-            fscanf(file, "%lu", &index);
-            sa->array[j] = index;
-        }
-        records->suffix_arrays[i] = sa;
         
-#if 0
-        const char *string = fasta_records->sequences->strings[i];
-        fprintf(stderr, "suffix array:\n");
-        for (int j = 0; j < sa->length; j++) {
-            fprintf(stderr, "sa[%3d] == %4lu %s\n",
-                    j, sa->array[j], string + sa->array[j]);
-        }
-#endif
+        sa->length = seq_length + 1;
+        sa->array = malloc(sizeof(size_t) * sa->length);
+    
+        fprintf(stderr, "Reading suffix arrays from %s [length %lu].\n",
+                filename, sa->length);
+        fread(sa->array, sizeof(size_t), sa->length, file);
+        
+        fclose(file);
+        free(filename);
 
     }
     
-    fclose(file);
-    free(filename);
-    
+
     fprintf(stderr, "Done.\n");
     
-    
-
     read_c_table_records(records, fasta_records, filename_prefix);
     read_o_table_records(records, fasta_records, filename_prefix);
-    
+
     return 0;
 }
 
