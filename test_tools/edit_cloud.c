320ac81351cfd29e983a896a8c408e32a798e7ae
Thomas Mailund <mailund@birc.au.dk>
2018-03-15 16:49:10 +0100

    reformatted code

--- test_tools/edit_cloud.c
+++ test_tools/edit_cloud.c
@@ -1,20 +1,18 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "edit_distance_generator.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-static void callback(const char *string, const char *cigar, void * data)
-{
+static void callback(const char *string, const char *cigar, void *data) {
     printf("%s %s\n", string, cigar);
 }
 
-int main(int argc, const char **argv)
-{
+int main(int argc, const char **argv) {
     if (argc != 4) {
         fprintf(stderr, "Usage: %s distance alphabet string\n", argv[0]);
         return EXIT_FAILURE;
     }
-    
+
     generate_all_neighbours(argv[3], argv[2], atoi(argv[1]), callback, 0);
-    
+
     return EXIT_SUCCESS;
 }
